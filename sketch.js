598db51a521f684f45a7d9965255b51bb566e6c6
SidSSid <122497481+SidSSid@users.noreply.github.com>
2023-01-11 20:13:52 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var box; 
+function setup() {
+  createCanvas(400, 400);
+  box = createSprite(200,200,30,30)
+}
+
+function draw() {
+  background(20);
+
+  if(keyDown (RIGHT_ARROW)){
+    box.position.x = box.position.x + 5;
+  }
+
+  if(keyDown (LEFT_ARROW)){
+    box.position.x = box.position.x - 5;
+  }
+
+  drawSprites();
+}
